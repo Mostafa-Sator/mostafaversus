d332caad21aeba6f968789a9898ee995b92b366a
Mostafa Sator <you@example.com>
2023-09-27 01:47:30 +0300

    first commit

---
+++ components/hero.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+const Navbar = () => (
+  
+);
+
+export default Navbar;
